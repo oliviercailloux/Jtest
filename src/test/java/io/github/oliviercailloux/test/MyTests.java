6d607197d4475026942c85533c2d020094be074d
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-03-21 18:38:32 +0100

    Simpler

--- src/test/java/io/github/oliviercailloux/test/MyTests.java
+++ src/test/java/io/github/oliviercailloux/test/MyTests.java
@@ -2,11 +2,9 @@ package io.github.oliviercailloux.test;
 
 import java.io.StringReader;
 import javax.xml.transform.stream.StreamSource;
-import org.junit.jupiter.api.Test;
 
 public class MyTests {
 
-  @Test
   void testSomething() throws Exception {
     new StreamSource(new StringReader(""));
   }
