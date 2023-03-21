936db8f8317521dcb4fe800d04d888335864d770
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-03-21 18:35:08 +0100

    Simplified

--- src/test/java/io/github/oliviercailloux/test/MyTests.java
+++ src/test/java/io/github/oliviercailloux/test/MyTests.java
@@ -3,16 +3,11 @@ package io.github.oliviercailloux.test;
 import java.io.StringReader;
 import javax.xml.transform.stream.StreamSource;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MyTests {
-  @SuppressWarnings("unused")
-  private static final Logger LOGGER = LoggerFactory.getLogger(MyTests.class);
 
   @Test
   void testSomething() throws Exception {
-    LOGGER.info("Started tests.");
     new StreamSource(new StringReader(""));
   }
 }
